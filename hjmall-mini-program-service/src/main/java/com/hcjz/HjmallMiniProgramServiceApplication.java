9c95b36f66fd71423ce3ad4be9102f57f5f4dc4e
Admin <git@192.168.0.109:root/test99.git>
2019-04-17 17:19:16 +0800

    hjmall-mini-program项目service启动类继承SpringBootServletInitializer

--- hjmall-mini-program-service/src/main/java/com/hcjz/HjmallMiniProgramServiceApplication.java
+++ hjmall-mini-program-service/src/main/java/com/hcjz/HjmallMiniProgramServiceApplication.java
@@ -4,6 +4,8 @@ import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
@@ -11,12 +13,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //@ComponentScan(basePackages = "com.hcjz.service")
 @EnableDubbo
 //@EnableWebMvc
-public class HjmallMiniProgramServiceApplication {
+public class HjmallMiniProgramServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HjmallMiniProgramServiceApplication.class, args);
     }
 
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HjmallMiniProgramServiceApplication.class);
+    }
 }
