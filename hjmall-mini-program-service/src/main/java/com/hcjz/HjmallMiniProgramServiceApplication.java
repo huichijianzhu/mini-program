fb0f29bad24f94386a480c4cfbe2fe316b44fad4
Admin <git@192.168.0.109:root/test99.git>
2019-04-20 16:16:38 +0800

    bug

--- hjmall-mini-program-service/src/main/java/com/hcjz/HjmallMiniProgramServiceApplication.java
+++ hjmall-mini-program-service/src/main/java/com/hcjz/HjmallMiniProgramServiceApplication.java
@@ -1,12 +1,10 @@
 package com.hcjz;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.hcjz.mapper")
